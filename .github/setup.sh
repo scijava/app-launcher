66defc860c34593bd2d3a3da3e0e1266f06edfb0
Curtis Rueden <ctrueden@wisc.edu>
2024-10-20 19:25:37 -0500

    CI: test on all three major platforms

--- .github/setup.sh
+++ .github/setup.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/main/ci-setup-github-actions.sh
 sh ci-setup-github-actions.sh
+
+if [ "$(uname)" != Linux ]
+then
+  echo "No deploy -- non-Linux build"
+  echo "NO_DEPLOY=1" >> $GITHUB_ENV
+fi
