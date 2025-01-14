42bb9b28d2573c2c38f3918123b9d8b5456a9b63
Curtis Rueden <ctrueden@wisc.edu>
2025-01-14 14:25:23 -0600

    Update license blurbs for 2025

--- src/main/java/org/scijava/launcher/ClassLoaders.java
+++ src/main/java/org/scijava/launcher/ClassLoaders.java
@@ -2,7 +2,7 @@
  * #%L
  * Launcher for SciJava applications.
  * %%
- * Copyright (C) 2007 - 2024 SciJava developers.
+ * Copyright (C) 2007 - 2025 SciJava developers.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
